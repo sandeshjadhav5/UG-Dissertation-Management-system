e29005a219ec760828f2524bf56936fc0dc9b243
sandeshjadhav5 <jadhavs932@gmail.com>
2024-01-01 23:55:43 +0530

    added signup component

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
-const SignUp = () => {
-  return <div></div>;
-};
+import { Link, useNavigate } from "react-router-dom";
+export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const toast = useToast();
 
-export default SignUp;
+  const handleSubmit = () => {
+    const payload = {
+      email,
+      password,
+    };
+  };
+
+  return (
+    <>
+      <Flex
+        minH={"90vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"}>Login</Heading>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}
+          >
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button
+                  w="100%"
+                  mt="5"
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.700",
+                  }}
+                  onClick={handleSubmit}
+                >
+                  Sign in
+                </Button>
+              </FormControl>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    </>
+  );
+}
