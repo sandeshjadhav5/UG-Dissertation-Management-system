3335359a215efd96a27e39bd7fa4436260cdb51c
sandeshjadhav5 <jadhavs932@gmail.com>
2024-01-01 23:57:36 +0530

    added some states in signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -16,6 +16,8 @@ import { useState } from "react";
 
 import { Link, useNavigate } from "react-router-dom";
 export default function SignUp() {
+  const [name, setName] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
