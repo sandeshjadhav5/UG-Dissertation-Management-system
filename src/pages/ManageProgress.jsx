11d29d6b53805dfe72a03a818dc1b44ff1e3e198
sandeshjadhav5 <jadhavs932@gmail.com>
2024-04-29 23:39:36 +0530

    added manage progress page

--- src/pages/ManageProgress.jsx
+++ src/pages/ManageProgress.jsx
@@ -18,18 +18,51 @@ import {
   useToast,
   Spacer,
   IconButton,
+  VStack,
+  FormControl,
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { FaAddressBook, FaEnvelope, FaEdit, FaTrash } from "react-icons/fa";
 import Navbar from "../components/Navbar";
 
 const ManageProgress = () => {
+  const [projectStatus, setProjectStatus] = useState("");
+  const toast = useToast();
+  const userId = localStorage.getItem("userId") || null;
+  const handleSubmit = async (e) => {
+    const payload = {
+      projectStatus: projectStatus,
+    };
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `http://localhost:8000/users/update-project-status/${userId}`,
+        payload
+      );
+      if (response.status === 200) {
+        toast({
+          title: "Project status updated",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
 
+      // Reset form fields
+      setProjectStatus("");
+    } catch (error) {
+      // Show error message
+      toast({
+        title: "Error",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
 
-
-  useEffect(() => {
-   
-  }, []);
+  useEffect(() => {}, []);
   return (
     <div>
       <Box>
@@ -50,8 +83,32 @@ const ManageProgress = () => {
         >
           <Navbar />
           {/* -------------------------------- YOUR GUIDE STARTS HERE ---------------------------- */}
-          <Box p="4">
-           
+          <Box
+            mt="8"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <VStack spacing={8}>
+              <form onSubmit={handleSubmit}>
+                <FormControl id="projectStatus">
+                  <FormLabel>Project Status</FormLabel>
+                  <Select
+                    value={projectStatus}
+                    onChange={(e) => setProjectStatus(e.target.value)}
+                    required
+                  >
+                    <option value="">Select project status</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                    <option value="On Hold">On Hold</option>
+                  </Select>
+                </FormControl>
+                <Button mt="4" type="submit" colorScheme="blue" width="100%">
+                  Update Status
+                </Button>
+              </form>
+            </VStack>
           </Box>
         </Box>
       </Box>
